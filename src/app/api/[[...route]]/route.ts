38e49958db5cef601cd345dc45c31ed3f31cd665
wahyu agus arifin <>
2025-01-20 13:48:19 +0700

    ex

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,10 +1,13 @@
 import HealthController from "@/server/controllers/health.controller";
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
+import ex from "@/utils/ex";
 
 const main = new Hono().get("/health", HealthController);
-const app = new Hono().basePath("/api").route("/main", main);
 
+const app = new Hono().basePath("/api")
+    .route("", ex)
+    .route("/main", main);
 export const GET = handle(app);
 export const POST = handle(app);
 export type AppType = typeof app;
